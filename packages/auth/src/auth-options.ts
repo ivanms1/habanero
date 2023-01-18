24eb3fdf0ff3ec2f1c37834f2861a5b634256aa8
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2023-01-18 17:44:50 +0530

    Feat/setup auth (#11)
    * setup sign in page
    
    * finish configuring google provider with next auth

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,5 +1,5 @@
 import { type NextAuthOptions } from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
+import GoogleProvider from 'next-auth/providers/google';
 
 import { prisma } from 'db';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
@@ -8,17 +8,20 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   callbacks: {
-    session({ session, user }: any) {
+    async session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
       }
       return session;
     },
+  },
+  pages: {
+    signIn: '/signin',
   },
 };
